9d32d100ef245dfb337795e4b7b61f8e5021bf09
andreneto97 <andreneto97@hotmail.com>
2021-11-23 12:34:29 -0300

    Change entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/sh -l
 
 echo "Hello $1"
-time=$(date)
-echo "::set-output name=time::$time"
+ls
+echo "Tar the Project"
+touch workspace.tar.gz
+tar -czf workspace.tar.gz --exclude=workspace.tar.gz .
+echo "Check Tar file"
+ls
+echo "Do a GET"
+curl --location --request GET 'https://jsonplaceholder.typicode.com/todos/1'
