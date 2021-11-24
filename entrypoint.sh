80bd76f04a0a61595209007434f1744ac9bb9642
André Neto <andreneto97@hotmail.com>
2021-11-24 16:01:29 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,23 +2,20 @@
 apk add curl
 apk add bash
 
-echo "Hello $1"
-ls
+# Transform project in tar files
 echo "Tar the Project"
 touch workspace.tar.gz
 tar -czf workspace.tar.gz --exclude=workspace.tar.gz .
-echo "Check Tar file"
-ls
-echo "Do a GET"
-curl -s 'https://jsonplaceholder.typicode.com/todos/1'
+
+# Send the tar file to Dashboard
 ls
 echo "Do a POST"
 curl --location --request POST 'https://jsonplaceholder.typicode.com/posts' \
           --form '=@"./workspace.tar.gz"'
 
+# Make a comment inside the pull request
 echo "token: $2"
 curl --location --request POST 'https://api.github.com/repos/andreneto97/react-example-action/issues/6/comments' \
           --header 'Authorization: Bearer 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' \
           --header 'Content-Type: application/json' \
           --data-raw '{"body": "Hello Github"}'
-  
